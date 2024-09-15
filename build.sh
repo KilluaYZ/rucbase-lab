4472cd4afd96fac7649dc211ada8a913cbb52bc8
killuayz <2959243019@qq.com>
2024-09-15 07:04:36 +0000

    补充完disk_manager，添加build脚本

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+ROOT_PATH=$(dirname $(readlink -f $0))
+
+# 编译server
+rm -r $ROOT_PATH/build
+mkdir $ROOT_PATH/build
+cd $ROOT_PATH/build
+cmake -DCMAKE_BUILD_TYPE=Debug ..
+make rmdb -j
+
+# 编译client
+CLIENT_ROOT=$ROOT_PATH/rucbase_client
+rm -r $CLIENT_ROOT/build
+mkdir $CLIENT_ROOT/build
+cd $CLIENT_ROOT/build
+cmake -DCMAKE_BUILD_TYPE=Debug ..
+make -j
--- src/storage/disk_manager.cpp
+++ src/storage/disk_manager.cpp
@@ -31,7 +31,9 @@ void DiskManager::write_page(int fd, page_id_t page_no, const char *offset, int 
     // 1.lseek()定位到文件头，通过(fd,page_no)可以定位指定页面及其在磁盘文件中的偏移量
     // 2.调用write()函数
     // 注意write返回值与num_bytes不等时 throw InternalError("DiskManager::write_page Error");
-
+    
+    if(lseek(fd, page_no * PAGE_SIZE, SEEK_SET) == -1) throw InternalError("DiskManager::write_page Error");
+    if(write(fd, offset, num_bytes) != num_bytes) throw InternalError("DiskManager::write_page Error");
 }
 
 /**
@@ -47,6 +49,9 @@ void DiskManager::read_page(int fd, page_id_t page_no, char *offset, int num_byt
     // 2.调用read()函数
     // 注意read返回值与num_bytes不等时，throw InternalError("DiskManager::read_page Error");
 
+    if(lseek(fd, page_no * PAGE_SIZE, SEEK_SET) == -1) throw InternalError("DiskManager::read_page Error");
+    if(read(fd, offset, num_bytes) != num_bytes) throw InternalError("DiskManager::read_page Error");
+    
 }
 
 /**
@@ -102,6 +107,10 @@ void DiskManager::create_file(const std::string &path) {
     // Todo:
     // 调用open()函数，使用O_CREAT模式
     // 注意不能重复创建相同文件
+    if(access(path.c_str(), F_OK) != -1) return;
+    int fd = open(path.c_str(), O_CREAT | O_WRONLY, 0644);
+    if(fd == -1) throw InternalError("DiskManager::create_file Error fail to create file"); 
+    close(fd);
 }
 
 /**
@@ -112,7 +121,8 @@ void DiskManager::destroy_file(const std::string &path) {
     // Todo:
     // 调用unlink()函数
     // 注意不能删除未关闭的文件
-    
+    if(path2fd_.find(path) != path2fd_.end()) return;
+    unlink(path.c_str());
 }
 
 
@@ -125,7 +135,12 @@ int DiskManager::open_file(const std::string &path) {
     // Todo:
     // 调用open()函数，使用O_RDWR模式
     // 注意不能重复打开相同文件，并且需要更新文件打开列表
-
+    if(path2fd_.find(path) != path2fd_.end()) throw InternalError("DiskManager::open_file Error file already opened");
+    int fd = open(path.c_str(), O_RDWR);
+    if(fd == -1) throw InternalError("DiskManager::open_file Error failed to open file");
+    path2fd_[path] = fd;
+    fd2path_[fd] = path;
+    return fd;
 }
 
 /**
@@ -136,7 +151,11 @@ void DiskManager::close_file(int fd) {
     // Todo:
     // 调用close()函数
     // 注意不能关闭未打开的文件，并且需要更新文件打开列表
-
+    if(fd2path_.find(fd) == fd2path_.end()) throw InternalError("DiskManager::close_file Error file is not open");
+    if(close(fd) == -1) throw InternalError("DiskManager::close_file Error fail to close file");
+    auto path = fd2path_[fd];
+    fd2path_.erase(fd);
+    path2fd_.erase(path);
 }
 
 
